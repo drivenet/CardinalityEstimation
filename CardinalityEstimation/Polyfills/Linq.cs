75a898342c4964cffeaf803da296112fb1f432da
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-09-20 21:51:46 +0100

    build: add netstandard2.0 build support

---
+++ CardinalityEstimation/Polyfills/Linq.cs
@@ -0,0 +1,13 @@
+﻿#if !NETCOREAPP2_1_OR_GREATER
+using System.Collections.Generic;
+
+namespace System.Linq;
+
+public static class LinqExtensions
+{
+    public static HashSet<T> ToHashSet<T>(this IEnumerable<T> source)
+    {
+        return new(source);
+    }
+}
+#endif
