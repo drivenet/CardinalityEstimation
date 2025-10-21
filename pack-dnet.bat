082c06b45ae98c920fa714d52bbc5018429b18fb
Aristarkh Zagorodnikov <xm@x-infinity.com>
2025-10-21 21:28:30 +0100

    build: bump version

--- pack-dnet.bat
+++ pack-dnet.bat
@@ -1,1 +1,1 @@
-dotnet build -c Release CardinalityEstimation/CardinalityEstimation.csproj /p:CurrentVersion=1.14.0.1 /p:PackageId=Dnet.CardinalityEstimation /p:ContinuousIntegrationBuild=true
+dotnet build -c Release CardinalityEstimation/CardinalityEstimation.csproj /p:CurrentVersion=1.14.0.2 /p:PackageId=Dnet.CardinalityEstimation /p:ContinuousIntegrationBuild=true
